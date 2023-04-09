0a20f9d6da81e0fde61e567779cd1b457688226b
amitranjan-oneclick <amit.ranjan@oneclicklca.com>
2023-04-09 13:44:03 +0530

    new chnages

--- src/components/HoldingBarChart.js
+++ src/components/HoldingBarChart.js
@@ -1,53 +1,53 @@
 import React from "react";
 
 import {
-    BarChart,
-    Bar,
-    Brush,
-    ReferenceLine,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend,
-    ComposedChart,
-    Line
-  } from "recharts";
+  BarChart,
+  Bar,
+  Brush,
+  ReferenceLine,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ComposedChart,
+  Line
+} from "recharts";
 
-export default function HoldingBarChart (props) {
-    const {holdingData} = props;
+export default function HoldingBarChart(props) {
+  const { holdingData } = props;
 
-      return (
-        <>
-        <ComposedChart
-          width={500}
-          height={400}
-          data={holdingData}
-          barSize ={15}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 20,
-            bottom: 5
-          }}
-        >
-          <CartesianGrid stroke="#f5f5f5" />
-          <XAxis dataKey="date" scale="band"/>
-          <YAxis />
-          <Tooltip />
-          <Legend verticalAlign="top" wrapperStyle={{ lineHeight: "40px" }} />
-          <ReferenceLine y={0} stroke="#000" />
-          <Brush dataKey="name" height={30} stroke="#8884d8" />
-          <Bar dataKey="totalBuyValue" fill="#82ca9d" name="Buy Value"/>
-          <Bar dataKey="totalPortfolioValue" fill="#8884d8" name="Portfolio Value"/>
-          <Line type="monotone" dataKey="totalPortfolioValue" stroke="#0693e3" name="Portfolio"/>
-
-      </ComposedChart>
-        <BarChart
+  return (
+    <div>
+      <ComposedChart
         width={500}
         height={400}
         data={holdingData}
-        barSize ={15}
+        barSize={15}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid stroke="#f5f5f5" />
+        <XAxis dataKey="date" angle={-30} interval={0} tick={{ fontSize: 'x-small', fontWeight: 'bold' }}/>
+        <YAxis />
+        <Tooltip />
+        <Legend verticalAlign="top" wrapperStyle={{ lineHeight: "40px" }} />
+        <ReferenceLine y={0} stroke="#000" />
+        {/* <Brush dataKey="name" height={30} stroke="#8884d8" /> */}
+        {/* <Bar dataKey="totalBuyValue" fill="#82ca9d" name="Buy Value" /> */}
+        <Bar dataKey="totalPortfolioValue" fill="#8884d8" name="Portfolio Value"/>
+        <Line type="monotone" dataKey="totalPortfolioValue" stroke="#0693e3" name="Portfolio" />
+      </ComposedChart>
+
+      <ComposedChart
+        width={500}
+        height={400}
+        data={holdingData}
+        barSize={15}
         margin={{
           top: 10,
           right: 30,
@@ -56,17 +56,17 @@ export default function HoldingBarChart (props) {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
+        <XAxis dataKey="date" interval={0} tick={{ fontSize: 'x-small', fontWeight: 'bold' }}/>
         <YAxis />
         <Tooltip />
         <Legend verticalAlign="top" wrapperStyle={{ lineHeight: "40px" }} />
         <ReferenceLine y={0} stroke="#000" />
-        <Brush dataKey="name" height={30} stroke="#8884d8" />
-        <Bar dataKey="dayChange" fill="#82ca9d" />
+        {/* <Brush dataKey="name" height={30} stroke="#8884d8" /> */}
+        {/* <Bar dataKey="dayChange" fill="#82ca9d" /> */}
         <Bar dataKey="dayChgPct" fill="#8884d8" />
-        {/* <Line type="monotone" dataKey="dayChgPct" stroke="#ff7300" /> */}
+        <Line type="monotone" dataKey="dayChgPct" stroke="#ff7300" />
 
-      </BarChart>
-      </>
-      );
+      </ComposedChart>
+    </div>
+  );
 }
