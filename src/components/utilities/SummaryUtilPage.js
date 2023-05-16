8ad2f11b6f954ff216b164bef6a3763d69d072ee
Praveen Ranjan <praveen.ranjan@oneclicklca.com>
2023-05-16 22:49:48 +0530

    Added userId in resetSummary

--- src/components/utilities/SummaryUtilPage.js
+++ src/components/utilities/SummaryUtilPage.js
@@ -21,7 +21,7 @@ export default function SummaryUtilPage(props) {
     }
 
     const resetSummary = () => {
-        resetSummaryData();
+        resetSummaryData(userId);
     }
 
     const updateStopLoss = () => {
